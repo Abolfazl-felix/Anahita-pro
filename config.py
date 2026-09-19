54bc65cabd76d826edd5da15d282c7402009edbb
Abolfazl-felix <abolfazl.b.h.1386.25@gmail.com>
2026-09-19 13:25:35 +0330

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,9 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 ADMIN_ID = 6771695421
 
-DATA_FILE = "events.json"
+TIMEZONE = "Asia/Tehran"
+
+DEFAULT_MESSAGE = (
+    "{emoji} تا {title} "
+    "{days} روز مونده ❤️"
+)
