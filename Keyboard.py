d205d8026b022559a34cde01f7067f5ce0aee827
Abolfazl-felix <abolfazl.b.h.1386.25@gmail.com>
2026-09-19 13:26:11 +0330

    Update Keyboard.py

--- Keyboard.py
+++ Keyboard.py
@@ -1,1 +1,41 @@
-#بعدا تکیمل میکنم
+from telegram import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup
+)
+
+
+def main_menu():
+
+    keyboard = [
+
+        [
+            InlineKeyboardButton(
+                "➕ افزودن مناسبت",
+                callback_data="add"
+            )
+        ],
+
+        [
+            InlineKeyboardButton(
+                "📋 لیست مناسبت‌ها",
+                callback_data="list"
+            )
+        ],
+
+        [
+            InlineKeyboardButton(
+                "📢 کانال‌ها",
+                callback_data="channels"
+            )
+        ],
+
+        [
+            InlineKeyboardButton(
+                "⚙️ تنظیمات",
+                callback_data="settings"
+            )
+        ],
+
+    ]
+
+    return InlineKeyboardMarkup(keyboard)
